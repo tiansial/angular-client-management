62729213fa14752e0c09e11ae2d851f149c8f213
tiansial <bomdia@tiansial.com>
2015-12-31 10:59:19 +0000

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularClientManagementApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularClientManagementApp
+ */
+angular.module('angularClientManagementApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularClientManagementApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularClientManagementApp
+ */
+angular.module('angularClientManagementApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
